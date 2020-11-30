d050f0e71aa3787a9ace791dbdf80ccd2f326658
Ronen <RonenNess@gmail.com>
2020-11-30 21:50:45 +0200

    changed 1.3.5 to 1.4 because I messed up the versioning in nuget by having 1.3.31

--- BonEngineSharp/Source/BonEngine.cs
+++ BonEngineSharp/Source/BonEngine.cs
@@ -17,7 +17,7 @@ namespace BonEngineSharp
         /// <summary>
         /// BonEngine version - must match the underlying CPP version.
         /// </summary>
-        public static float Version => 1.35f;
+        public static float Version => 1.4f;
 
         // did we call 'Start' already?
         static bool _wasInit;
